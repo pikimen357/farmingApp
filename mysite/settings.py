e7fc7a17b25a2e32aa366227dc3db213519b0ed3
VICKY3824D <pradana2766@gmail.com>
2025-01-27 16:18:20 +0700

    update default pagination class

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES" : [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ], 
-    "DEFAULT_PAGINATON_CLASS" : 'rest_framework.pagination.LimitOffsetPagination',
-    "PAGE_SIZE" : 100
+    "DEFAULT_PAGINATION_CLASS" :"rest_framework.pagination.LimitOffsetPagination",
+                                "PAGE_SIZE" : 10
 }
 
