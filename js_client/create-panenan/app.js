564a8cfe7019f1d15c3d92928006b3b296b31206
VICKY3824D <pradana2766@gmail.com>
2025-02-12 08:03:51 +0700

    update create-panenan js

--- js_client/create-panenan/app.js
+++ js_client/create-panenan/app.js
@@ -1,4 +1,5 @@
 const panenanForm = document.getElementById('panenanForm');
+const dataTanaman = document.getElementById('tanaman-list');
 
 function isTokenNotValid(jsonData) {
     if(jsonData.code && jsonData.code === 'token_not_valid'){
@@ -13,6 +14,47 @@ function isTokenNotValid(jsonData) {
 
  // get access token from local(chaching)
  const authToken = localStorage.getItem('access');
+
+const options = {
+    method: "GET",
+    headers: {
+        "Content-Type" : "application/json",
+        "Authorization" : `Bearer ${authToken}`
+    }
+};
+
+
+// get all tanaman, then add to <datalist>
+ fetch('http://127.0.0.1:8000/v3/tanaman/', options)    
+ .then((response) => {
+    console.log("respon e: ",response);
+    return response.json();
+})
+.then((data) => {
+    console.log(data.results);
+    const isValidData = isTokenNotValid(data);
+
+    if (isValidData && data.results){
+        let htmlStr = "";
+        for (let result of data.results){   
+            console.log(result);
+            htmlStr +=  `
+                <option value="${result.id}">${result.nama_tanaman}</option>
+        `;
+        }
+        dataTanaman.innerHTML = htmlStr
+        if (data.results.length === 0){
+            dataTanaman.innerHTML = '<option value="Tidak ada tanaman"></option>'
+        }
+    }
+    else {
+        dataTanaman.innerHTML = '<option value="Tidak ada tanaman"></option>'
+    }
+
+})
+.catch(error => alert('Terjadi kesalahan: ' + error));
+
+
 
  panenanForm.addEventListener('submit', function(event) {
     event.preventDefault();
