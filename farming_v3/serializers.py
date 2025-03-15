2b8dad40f9905faee63b5dca69c38764e760e96c
VICKY3824D <pradana2766@gmail.com>
2025-03-15 14:24:54 +0700

    fix immutable dict

--- farming_v3/serializers.py
+++ farming_v3/serializers.py
@@ -73,15 +73,15 @@ class TanamanSerializer(serializers.ModelSerializer):
         
         return reverse('tanaman-edit', kwargs={'nama_tanaman' : obj.nama_tanaman}, request=request)
     
-    def to_internal_value(self, data):
+    # def to_internal_value(self, data):
         
-        # ensure certain fields are converted to integer
-        data["owner"] = int(data["owner"]) if "owner" in data else None
-        data["waktu_tanam_hari"] = int(data["waktu_tanam_hari"]) if "waktu_tanam_hari" in data else None
-        data["harga_per_ton"] = int(data["harga_per_ton"]) if "harga_per_ton" in data else None
-        data["peluang_hama"] = int(data["peluang_hama"]) if "peluang_hama" in data else None
+    #     # ensure certain fields are converted to integer
+    #     data["owner"] = int(data["owner"]) if "owner" in data else None
+    #     data["waktu_tanam_hari"] = int(data["waktu_tanam_hari"]) if "waktu_tanam_hari" in data else None
+    #     data["harga_per_ton"] = int(data["harga_per_ton"]) if "harga_per_ton" in data else None
+    #     data["peluang_hama"] = int(data["peluang_hama"]) if "peluang_hama" in data else None
         
-        return super().to_internal_value(data)
+    #     return super().to_internal_value(data)
     
     def validate_waktu_tanam_hari(self, value): 
         if value <= 0:
